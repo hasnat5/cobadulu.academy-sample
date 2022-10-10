d32e29ff198e11c70bda4569ee683d4ab1b66808
hasnat5 <hasnatferdiananda5@gmail.com>
2022-10-10 21:18:39 +0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,28 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./client/*.{html,js}'],
+  theme: {
+    extend: {
+      fontFamily: {
+        Montserrat: ['Montserrat'],
+        Inter: ['Inter'],
+      },
+
+      backgroundImage: {
+        'main': "url('../asset/jumbotron.png')",
+        'login': "url('../asset/login.jpg')",
+        'signup': "url('../asset/sign up.jpg')",
+      },
+
+      colors: {
+        primary: '#171430',
+        secondary: '#24204B',
+        CTA: '#0053FB',
+        CTAsec: '#3396CD',
+        banger: '#F3583B',
+        third: '#736EAA',
+      }
+    }
+  },
+  plugins: [],
+}
