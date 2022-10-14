9eda0cad65e93c02cf9ac11c1ed8bf980d95676d
hasnat5 <hasnatferdiananda5@gmail.com>
2022-10-14 10:03:25 +0700

    add slider

--- client/script.js
+++ client/script.js
@@ -10,3 +10,37 @@ document.addEventListener('scroll', function () {
         navBar.classList.remove('navBg')
     }
 });
+
+var swiper = new Swiper(".slide-content", {
+    slidesPerView: 3,
+    slidesPerGroup: 3,
+    spaceBetween: 25,
+    loop: false,
+    centerSlide: 'true',
+    fade: 'true',
+    grabCursor: 'true',
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+        dynamicBullets: true,
+    },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+
+    breakpoints: {
+        0: {
+            slidesPerView: 1,
+            slidesPerGroup: 1,
+        },
+        520: {
+            slidesPerView: 2,
+            slidesPerGroup: 2,
+        },
+        950: {
+            slidesPerView: 3,
+            slidesPerGroup: 3,
+        },
+    },
+});
