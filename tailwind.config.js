fdadbca65a8f101045f8ce2ec050c6b5962a3d1d
hasnat5 <hasnatferdiananda5@gmail.com>
2022-10-14 14:30:07 +0700

    fix card

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,28 +1,29 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./client/*.{html,js}'],
-  theme: {
-    extend: {
-      fontFamily: {
-        Montserrat: ['Montserrat'],
-        Inter: ['Inter'],
-      },
+	content: ['./client/*.{html,js}'],
+	theme: {
+		extend: {
+			fontFamily: {
+				Montserrat: ['Montserrat'],
+				Inter: ['Inter'],
+			},
 
-      backgroundImage: {
-        'main': "url('../asset/jumbotron.png')",
-        'login': "url('../asset/login.jpg')",
-        'signup': "url('../asset/sign up.jpg')",
-      },
+			backgroundImage: {
+				'main': "url('../asset/jumbotron.png')",
+				'login': "url('../asset/login.jpg')",
+				'signup': "url('../asset/sign up.jpg')",
+				'mentor': "url('../asset/mentor.jpg')",
+			},
 
-      colors: {
-        primary: '#171430',
-        secondary: '#24204B',
-        CTA: '#0053FB',
-        CTAsec: '#3396CD',
-        banger: '#F3583B',
-        third: '#736EAA',
-      }
-    }
-  },
-  plugins: [],
+			colors: {
+				primary: '#171430',
+				secondary: '#24204B',
+				CTA: '#0053FB',
+				CTAsec: '#3396CD',
+				banger: '#F3583B',
+				third: '#736EAA',
+			}
+		}
+	},
+	plugins: [],
 }
